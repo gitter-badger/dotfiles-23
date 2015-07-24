6ce22a9b1e02cf74caa63ec64c5c18d4be59197b
CutAndPastey <jon@ctrlxctrlv.co.uk>
2015-07-24 15:56:37 +0100

    update setup init

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+HAS_APT=`which apt-get`;
+
+#If we are in the dotfiles project file
 if [ -d ./lib  ]; then
-	if [ -d ~/.dotfiles ];then
-		echo 'Got a directory';
+	#Remove previous link	
+	if [ -d ~/.dotfiles ]; then
+		rm -rf ~/.dotfiles
 	fi 
-#else
+	#Link this folder to the home/.dotfiles	
+	ln -s `pwd` ~/.dotfiles	
+
+#If we are using curl
+else
+	if [ $HAS_APT ]; then
+		sudo spt-get install git-core;
+	fi
+	git clone git@github.com:cutandpastey/dotfiles.git ~/.dotfiles;
 fi
 
-#. ./lib/backup.sh
-#. ./lib/nvm.sh
+#Move into the dotfiles directory
+pushd ~/.dotfiles
+
+. ./lib/backup.sh
+. ./lib/nvm.sh
